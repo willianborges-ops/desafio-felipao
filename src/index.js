e62fed75efbc15d3109e99b4cf6c79c344cc7b32
willianborges-ops <williandev01@gmail.com>
2024-11-16 07:04:50 -0300

    meu ultimo commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Olá Mundo!");
+let NomeDoHeroi = "100XJ";
+let XpDoHeroi = 5001;
+if(XpDoHeroi<=1000) {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Ferro.`);
+}
+else if(XpDoHeroi <=2000) {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Bronze.`);
+}
+else if(XpDoHeroi <=5000) {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Prata.`);
+}
+else if(XpDoHeroi <=7000) {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Ouro.`);
+}
+else if(XpDoHeroi <=8000) {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Platina.`);
+}
+else if(XpDoHeroi <=9000) {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Ascendente.`);
+}
+else if(XpDoHeroi <=10000) {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Imortal.`);
+}
+else {
+console.log(`O Herói de nome ${NomeDoHeroi} está no nivel de Radiante.`);  
+}
+
+
